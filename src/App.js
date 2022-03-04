733832374cabd742f28ded71d5e5cfb13456b10b
Jesus Oviedo <joviedo1090@gmail.com>
2022-03-04 16:50:58 -0500

    Edited App.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Our shopping Experience
         </a>
       </header>
     </div>
